f011fa0723126056f42123d0d1fd2fd54e9192f4
Garvan Keeley <garvankeeley@gmail.com>
2016-04-22 11:48:11 -0400

    Rename to OpenInThirdPartyBrowser

--- OpenInThirdPartyBrowser/OpenInThirdPartyBrowserControllerObjC.m
+++ OpenInThirdPartyBrowser/OpenInThirdPartyBrowserControllerObjC.m
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #import <UIKit/UIKit.h>
-#import "OpenInFirefoxControllerObjC.h"
+#import "OpenInThirdPartyBrowserControllerObjC.h"
 
 static NSString *const firefoxScheme = @"firefox:";
 
